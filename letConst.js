6d2b7fcde101177eb11dedf6ae7ab66c7bda32d9
darklordsadaf <darklordsadaf@gmail.com>
2020-08-06 21:45:50 +0600

    file upload

---
+++ letConst.js
@@ -0,0 +1,11 @@
+const name = "Dom";
+console.log(name);
+
+const num = [10, 88];
+num [1] = 33;
+num.push(69);
+console.log(num); 
+
+let petName = "oreo";
+petName = "cheggy";
+console.log(petName);
